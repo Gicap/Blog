2798ccecbd83011b36052f550807645f60bcdbc4
Giacomo <giacomo.cappucci@gmail.com>
2014-11-05 14:53:43 +0100

    Ripristino mini-funcionality site

--- src/Bugiardini/BlogBundle/Controller/PageController.php
+++ src/Bugiardini/BlogBundle/Controller/PageController.php
@@ -15,38 +15,6 @@ class PageController extends Controller
 
     public function indexAction()
     {
-        $values = [1 => 1,
-            2 => 2,
-            3 => 'fizz',
-            4 => 4,
-            5 => 'buzz',
-            6 => 'fizz',
-            7 => 7,
-            8 => 8,
-            9 => 'fizz',
-            12 => 'fizz',
-            15 => 'fizzbuzz',
-            30 => 'fizzbuzz'];
-        $res = "";
-        foreach ($values as $value => $result) {
-            if ($this->fizzBuzz($value) === $result)
-            {
-                $res = $res . "ok<br>";}
-            else {
-                $res = $res . "NO<br>";
-            }
-        }
-        return $res;
-        //return fizzBuzzTest(); //$this->render('BugiardiniBlogBundle:Page:index.html.twig');
+        return $this->render('BugiardiniBlogBundle:Page:index.html.twig');
     }
-
-    private function fizzBuzz($f)
-    {
-        $r = '';
-        if ($f % 3 == 0) $r = 'fizz';
-        if ($f % 5 == 0) $r = $r . 'buzz';
-        if (strlen($r) < 4) $r = $f;
-        return $r;
-    }
-
 }
